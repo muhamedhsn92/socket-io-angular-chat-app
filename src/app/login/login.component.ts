d9061a2f486b6ade108e2ce34ed50c3830a0d47d
muhamed <muhamed.hassan92@gmail.com>
2021-03-29 16:56:34 +0200

    adding UUID to repo

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      idNumber: null
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  handleSubmit() {
+    console.log('form value', this.form.value)
+  }
+}
